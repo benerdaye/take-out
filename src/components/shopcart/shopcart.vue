c013d169d27c62366b76a65872a8d3de0b704766
二大爷 <793507471@qq.com>
2017-01-06 20:58:14 +0800

    完成shopcart组件

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="shopcart">
     <div class="content">
-      <div class="content-left">
+      <div class="content-left" :class="{'heightkight: totalCount > 0'}">
         <div class="logo-wrapper">
           <div class="logo">
             <i class="icon-shooping_cart"></i>
           </div>
+          <div class="num" v-show="totalCount > 0">{{totalCount}</div>
         </div>
-        <div class="price">￥ 0元</div>
+        <div class="price">￥ {{totalPrice}}元</div>
         <div class="desc">另需配送费￥ {{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">￥ {{minPrice}}元起送</div>
+        <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
   </div>
@@ -20,6 +21,12 @@
 <script type="texy/ecmascript-6">
   export default {
     props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return []
+        }
+      },
       deliveryPrice: {
         type: Number,
         default: 0
@@ -27,6 +34,34 @@
       minPrice: {
         type: Number,
         default: 0
+      }
+    },
+    computed: {
+      totalPrice () {
+        let total = 0
+        this.selectFoods.map((food) => {
+          total += food.price * food.count
+        })
+        return total
+      },
+      totalCount () {
+        let count=0
+        this.selectFoods.map((food) => {
+          count += food.count
+        })
+        return count
+      },
+      payDesc () {
+        if (this.tatalCount === 0) {
+          return `￥ ${this.minPrice}元起送`
+        } else if (this.tatalPrice < this.minPrice) {
+          return `还差￥ ${this.minPrice - this.tatalPrice}元起送`
+        }else {
+          return '去结算'
+        }
+      },
+      payClass () {
+        return (this.totalPrice < this.minPrice) ? 'not-enough' : 'enough'
       }
     }
   }
@@ -46,6 +81,14 @@
       color rgba(255, 255, 255, .4)
       .content-left
         flex 1
+        &.heightlight
+          .logo-wrapper
+            .logo
+              background rgb(0, 160, 220)
+              .icon-shopping_cart
+                color #fff
+          .price
+            color #fff
         .logo-wrapper
           display inline-block
           position relative
@@ -68,6 +111,20 @@
               line-height 44px
               font-style 24px
               color #80858a
+          .num
+            position absolute
+            top 0
+            right 0
+            width 24px
+            height 16px
+            line-height 1
+            text-align center
+            border-radius 16px
+            font-style 9px
+            font-weight 700
+            color #fff
+            background rgb(240, 20, 20)
+            box-shadow 0 4px 8px 0 rgba(0, 0, 0, .4)
         .price
           display inline-block
           vertical-align top
@@ -92,6 +149,10 @@
           line-height 48px
           text-align center
           font-weight 700
-          background #2b333b
+          &.not-enough
+            background #2b333b
+          &.enough
+            background #00b43c
+            color #fff
 
 </style>
