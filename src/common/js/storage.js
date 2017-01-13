55c1e094b03982cad303d27efb8cccb7fb5dd8a0
二大爷 <793507471@qq.com>
2017-01-13 09:32:06 +0800

    添加本地存储 的公共函数

---
+++ src/common/js/storage.js
@@ -0,0 +1,26 @@
+export function savTolocal (id, key, value) {
+  let seller = window.localStorage._seller_
+  if (!seller) {
+    seller = {}
+    seller[id] = {}
+  } else {
+    seller = JSON.parse(seller)
+    if (!seller[id]) {
+      seller[id] = {}
+    }
+  }
+  seller[id][key] = value
+  window.localStorage._seller_ = JSON.stringify(seller)
+}
+
+export function loadFromLocal (id, key, def) {
+  let seller = window.localStorage._seller_
+  if (!seller) {
+    return def
+  }
+  seller = JSON.parse(seller)[id]
+  if (!seller) {
+    return def
+  }
+  return seller[key] || def
+}
